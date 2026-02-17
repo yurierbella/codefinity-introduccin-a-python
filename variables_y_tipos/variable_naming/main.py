303920e43f1493250ae5c5005c2b3f2b635c7b69
yurierbella <>
2026-02-17 15:47:27 +0000

    Variable Naming

---
+++ variables_y_tipos/variable_naming/main.py
@@ -0,0 +1,9 @@
+item_name = "Bread"
+item_price = 4.52
+items_in_stock = 230
+
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
