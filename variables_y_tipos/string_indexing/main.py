39fa54102d4bee46fcb7ca262c31d2eaee6205d3
yurierbella <>
2026-02-18 15:06:09 +0000

    String Indexing

---
+++ variables_y_tipos/string_indexing/main.py
@@ -0,0 +1,13 @@
+grocery_item = "Grilled Chicken Salad"
+length_of_item = len(grocery_item)
+first_char = grocery_item[0]
+second_char = grocery_item[8]
+third_char = grocery_item[16]
+last_char1 = grocery_item[-15]
+last_char2 = grocery_item[-7]
+last_char3 = grocery_item[-1]
+
+# Testing
+print("Length of item name:", length_of_item)
+print("First character of each word:", first_char, second_char, third_char)
+print("Last character of each word:", last_char1, last_char2, last_char3)
